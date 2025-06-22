6d20d9d6c833287d9c2f813d3d353a5112bad5ae
jose-robertooliveira <negroafrolife@gmail.com>
2025-06-22 20:40:34 -0300

    some changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-echo ">>> Executando migrações Alembic..."
+echo ">>> Wait for database..."
+
+until uv run python -c "import asyncio; import asyncpg; asyncio.run(asyncpg.connect(dsn='${DATABASE_URL}'))"; do
+  echo "Database is not ready yet, run again in 2 seconds..."
+  sleep 2
+done
+
+echo ">>> Database is ready!"
+
+echo ">>> Alembic migrations..."
 uv run alembic upgrade head
 
-echo ">>> Subindo FastAPI no Uvicorn..."
+echo ">>> Run FastAPI on Uvicorn..."
 uv run uvicorn --host 0.0.0.0 --port 8000 fastapi_lesson.app:app
